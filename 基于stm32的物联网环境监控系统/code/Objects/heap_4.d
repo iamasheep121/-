45c74161146ba02ccea86f57cef1a73cf8d45acf
iamasheep121 <3069970600@qq.com>
2025-11-18 00:06:05 +0800

    初步移植freetos

---
+++ 基于stm32的物联网环境监控系统/code/Objects/heap_4.d
@@ -0,0 +1,14 @@
+.\objects\heap_4.o: Middlewares\FreeRTOS\portable\MemMang\heap_4.c
+.\objects\heap_4.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\heap_4.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\heap_4.o: .\Middlewares\FreeRTOS\include\FreeRTOS.h
+.\objects\heap_4.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\heap_4.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\heap_4.o: .\User\FreeRTOSConfig.h
+.\objects\heap_4.o: .\Middlewares\FreeRTOS\include\projdefs.h
+.\objects\heap_4.o: .\Middlewares\FreeRTOS\include\portable.h
+.\objects\heap_4.o: .\Middlewares\FreeRTOS\include\deprecated_definitions.h
+.\objects\heap_4.o: .\Middlewares\FreeRTOS\portable\RVDS\ARM_CM3\portmacro.h
+.\objects\heap_4.o: .\Middlewares\FreeRTOS\include\mpu_wrappers.h
+.\objects\heap_4.o: .\Middlewares\FreeRTOS\include\task.h
+.\objects\heap_4.o: .\Middlewares\FreeRTOS\include\list.h
